790718d6e28e10b80d7a3cf6bd82f349ff4ed43a
Tim Aspesberro <taspes@gmail.com>
2023-04-03 16:14:39 -0400

    added test files and started generateHTML

---
+++ src/generateHTML.js
@@ -0,0 +1,26 @@
+const generateEngineer = function () {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+            </div>
+        </div>
+    </div>
+            `;
+};
+
+const generateIntern = function () {
+    
+};
+
+const generateManager = function () {
+    
+};
+
+const generateHTML = function () {
+
+};
